3285c6c10f04f94126d1acd22f46ae0852dbd7dc
andyqmongo <andyqmongo@gapp.nthu.edu.tw>
2017-03-07 20:59:20 +0800

    decoder ver with slight modify

--- simulator/simulator.cpp
+++ simulator/simulator.cpp
@@ -5,28 +5,6 @@
 #include <bitset>
 #include <vector>
 
-class Inst{
-    //std::bitset<6> op;
-   // std::bitset<5> rs, rt, rd, shamt;
-   // std::bitset<6> funct;
-    int op, rs, rt, rd ,shamt, funct;
-public:
-    Inst(){
-        std::cout<<"didn't catch binary instruction."<<std::endl;
-    }
-    Inst(int bs){
-	op =  
-	rs = 
-	rd = 
-	shamt = 
-	funct = 
-    }
-    ~Inst();
-
-void decode(){
-    
-}
-}
 
 int main(){
    
@@ -35,10 +13,9 @@ int main(){
     std::ofstream snap("snapshot.rpt");
     std::ofstream err("erro_dump.rpt");
     
-    std::vector<std::string> str_ii;
-    std::vector<std::string> str_di;
-    //std::bitset<32> bitstring; 
-    
+    int ii_arr[1000];
+    int di_arr[1000];
+
     bool catchFile = true;
     if(!ii){
         std::cout<<"didn't catch iimage.bin"<<std::endl;
@@ -50,15 +27,10 @@ int main(){
     }
     if(!catchFile)
         return -1;
-    
-    //not yet
-    ii >> str_ii[0];//load initial state
-    ii >> str_ii[1];//load number of instructions
-    //not yet
-    std::bitset<32> bitstring (str_ii[1]);
-    int instrucNo = (int)(bitstring.to_ulong());
-    
-    
+   int init;
+   int commandNum;
+   
+   
 
 
 
